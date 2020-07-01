acf225cb24021e3e620996b35a25594cc660665a
memo <mehmetu1998@gmail.com>
2020-07-01 22:07:58 +0200

    main

--- src/System/main.cpp
+++ src/System/main.cpp
@@ -8,17 +8,18 @@ int main(int argc, char** argv)
     Invaders invaders;
     invaders.load_rom(argv[1]);
 
-    sf::RenderWindow window(sf::VideoMode(800,600), "spaceinvaders");
+    sf::RenderWindow window(sf::VideoMode(420,480), "spaceinvaders");
     window.setFramerateLimit(60);
+    window.setPosition(sf::Vector2i(500, 250));
     sf::Event event;
 
     while (window.isOpen())
     {
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == event.Closed)
                 window.close();
-            
+           
             invaders.handle_event(event);
         }
 
